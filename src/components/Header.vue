5b12b88a2f6cdb23b8bae9117cc9f2ad7ef13bf8
Szymon Gryszko <gryszkoszymon@gmail.com>
2021-05-29 13:05:51 +0200

    initial router setup

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header class="navbar navbar-expand-lg navbar-light bg-light">
+        <div>
+            <font-awesome-icon icon="user-secret" /><div class="display-4 font-weight-bold">MyMovies</div>
+        </div>
+    </header>
+    
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
